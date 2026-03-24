9fb47fe8acf5a757f009a06f0c9d62ad88e68c59
Acer <ann.mazur@outlook.com>
2026-03-24 10:03:15 +0000

    env and env in app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from flask import Flask
+# importing os module for environment variables
+import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv, dotenv_values
+# loading variables from .env file
+load_dotenv()
+
+# accessing and printing value
+print(os.getenv("MY_KEY"))
 
 app = Flask(__name__)
 
